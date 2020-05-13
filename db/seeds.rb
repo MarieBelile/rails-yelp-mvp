ecbfa41d95dee492e6aa280c72e1f130c625032d
Marie Belile <belilemarie@gmail.com>
2020-05-13 14:17:52 +0200

    ajout models reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+blend =  { name: "Blend", address: "Quai de Valmy", category: "french" }
+nopal =  { name: "Nopal", address: "Quai de Valmy", category: "french" }
+marcel = { name: "Marcel", address: "Quai de Valmy", category: "french" }
+
+[ dishoom, pizza_east, blend, nopal, marcel ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
